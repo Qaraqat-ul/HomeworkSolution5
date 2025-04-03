02445bedb756326d830a88645b20efabdbe91000
Qaraqat-ul <karakat.zhumagali@narxoz.kz>
2025-04-03 12:37:03 +0500

    Homework – 5

--- proxy_pattern/Main.java
+++ proxy_pattern/Main.java
@@ -1,1 +1,48 @@
 // Proxy Pattern 
+interface Image {
+    void display();
+}
+
+class RealImage implements Image {
+    private String filename;
+
+    public RealImage(String filename) {
+        this.filename = filename;
+        loadImage();
+    }
+
+    private void loadImage() {
+        System.out.println("Loading " + filename);
+    }
+
+    public void display() {
+        System.out.println("Displaying " + filename);
+    }
+}
+
+class ProxyImage implements Image {
+    private RealImage realImage;
+    private String filename;
+
+    public ProxyImage(String filename) {
+        this.filename = filename;
+    }
+
+    public void display() {
+        if (realImage == null) {
+            realImage = new RealImage(filename);
+        }
+        realImage.display();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Image img1 = new ProxyImage("house1.jpg");
+        Image img2 = new ProxyImage("house2.jpg");
+
+        img1.display();
+        img1.display();
+        img2.display();
+    }
+}
